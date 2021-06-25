bf4d3a3c3c5b1ad0db451665d4e574b0bb06ac0a
Thomas <thomas.bribet@gmail.com>
2021-06-25 15:58:14 +0200

    modal v1

--- marketfront/src/components/admin/Admin-modify-card.vue
+++ marketfront/src/components/admin/Admin-modify-card.vue
@@ -45,15 +45,30 @@
               <input type="number" v-model="cardResume.leveeFonds" id="leveeFonds"/>
             </div>
             <div class="detailsMarketPlace">
-              <label for="categorie">Catégorie :</label>
-              <!-- <select type="text" v-model="cardResume.categorie" id="categorie"> -->
-                <!-- <option value="dog">dog</option> -->
-                  <input type="text" name="example" list="exampleList">
-                <datalist id="exampleList">
-                  <option value="A">A</option>
-                  <option value="B">B</option>
-                </datalist>
-              <!-- </select> -->
+              <div class="categories">
+                <label for="categorie">Catégorie :</label>
+                <img src="../../assets/update.png" alt="" @click="$refs.modalName.openModal()">
+                <Modal ref="modalName">
+                  <template v-slot:header>
+                    <h1>Catégories</h1>
+                  </template>
+
+                  <template v-slot:body>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc sed velit dignissim sodales ut eu sem integer vitae. Id aliquet lectus proin nibh nisl condimentum. Fringilla urna porttitor rhoncus dolor purus. Nam aliquam sem et tortor. Nisl vel pretium lectus quam id. Cras pulvinar mattis nunc sed. Quis ipsum suspendisse ultrices gravida dictum fusce ut placerat orci. Tristique magna sit amet purus. Fermentum et sollicitudin ac orci phasellus egestas tellus. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan. Felis eget nunc lobortis mattis aliquam faucibus. Tincidunt eget nullam non nisi est sit amet facilisis. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Vitae proin sagittis nisl rhoncus mattis rhoncus urna neque. Eget nunc scelerisque viverra mauris in aliquam sem fringilla ut. Nec nam aliquam sem et tortor consequat id. Commodo nulla facilisi nullam vehicula ipsum a. Elementum tempus egestas sed sed. Faucibus purus in massa tempor nec feugiat nisl pretium fusce.</p>
+                  </template>
+
+                  <template v-slot:footer>
+                    <div>
+                      <button @click="$refs.modalName.closeModal()">Fermer</button>
+                    </div>
+                  </template>
+                </Modal>
+              </div>
+              <input type="text" name="example" list="exampleList">
+              <datalist id="exampleList">
+                <option value="A">A</option>
+                <option value="B">B</option>
+              </datalist>
             </div>
             <div class="detailsMarketPlace">
               <label for="url">Site internet :</label>
@@ -69,7 +84,11 @@
 
 <script>
 import axios from "axios";
+import Modal from "../layout/CategorieModal.vue"
 export default {
+  components: {
+    Modal,
+  },
   data() {
     return {
       cardResume: {
@@ -84,7 +103,7 @@ export default {
         imgSite1: "",
         imgSite2: ""
       },
-      idCard: this.$route.params.id
+      idCard: this.$route.params.id,
     };
   },
   mounted () {
@@ -100,10 +119,10 @@ export default {
         .then(() => {
             this.$router.push('/adminhome'); // redirection vers la page admin-home
         })
-      .catch(error => {
-        console.error(error);
-      })
-    }
+        .catch(error => {
+          console.error(error);
+        })
+      },
   },
 };
 </script>
@@ -196,7 +215,18 @@ export default {
 .detailsMarketPlace {
   display: flex;
   justify-content: space-between;
+  align-items: center;
   margin: 5% 0;
+}
+.detailsMarketPlace img {
+  width: 25px;
+  margin: 0;
+}
+.categories {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 30%;
 }
 p {
   background-color: var(--whiteCard);
---
+++ marketfront/src/components/layout/CategorieModal.vue
@@ -0,0 +1,70 @@
+<template>
+  <transition name="fade">
+    <div class="modal" v-if="show">
+      <div class="modal__dialog">
+          
+        <div class="modal__header">
+          <slot name="header"/>
+          <button type="button" class="modal__close" @click="closeModal()">
+            <img src="../../assets/moins.png" alt="">
+          </button>
+        </div>
+
+        <div class="modal__body">
+          <ul>
+              <li>
+                  <p></p>
+                  <button>
+                      <img src="../../assets/trash.png" alt="">
+                  </button>
+              </li>
+          </ul>
+        </div>
+
+        <div class="modal__footer">
+          <slot name="footer"/>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  data() {
+    return {
+      show: false
+    };
+  },
+  methods: {
+    closeModal() {
+      this.show = false;
+      document.querySelector("body").classList.remove("overflow-hidden");
+    },
+    openModal() {
+      this.show = true;
+      document.querySelector("body").classList.add("overflow-hidden");
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+.modal {
+  overflow-x: hidden;
+  overflow-y: auto;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 9;
+}
+
+.modal__body ul li {
+    display: flex;
+    justify-content: space-between;
+}
+</style>
