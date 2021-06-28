e193b0fa9fe166bb436efb83259c5f606277f734
fabienD31 <fabien.desnoues31@gmail.com>
2021-06-28 16:32:54 +0200

    css modal

--- marketfront/src/components/layout/CategorieModal.vue
+++ marketfront/src/components/layout/CategorieModal.vue
@@ -2,23 +2,30 @@
   <transition name="fade">
     <div class="modal radiusCard" v-if="show">
       <div class="modal__dialog">
-
         <div class="modal__body">
-          <div>
-            <input type="text" v-model="categorieResume.nom">
+          <div class="ajoutCat">
+            <input
+              type="text"
+              placeholder="ajouter une categorie"
+              v-model="categorieResume.nom"
+            />
             <button type="submit" @click="ajoutCat">Ajouter</button>
           </div>
-          <ul>
-              <li v-for="(cat, catIndex) in tabCat" :key="catIndex">
-                    <input type="text" v-model="cat.nom" />
-                    <p @click="modifierCat(cat._id)">Modifier</p>
-                    <img src="../../assets/trash.png" alt="" @click="suppCat(cat._id)">
-              </li>
+          <ul class="listeCat">
+            <li v-for="(cat, catIndex) in tabCat" :key="catIndex">
+              <input type="text" v-model="cat.nom" />
+              <button @click="modifierCat(cat._id)">Modifier</button>
+              <img
+                src="../../assets/trash.png"
+                alt=""
+                @click="suppCat(cat._id)"
+              />
+            </li>
           </ul>
         </div>
 
         <div class="modal__footer">
-          <slot name="footer"/>
+          <slot name="footer" />
         </div>
       </div>
     </div>
@@ -26,7 +33,7 @@
 </template>
 
 <script>
-import axios from "axios"
+import axios from "axios";
 
 export default {
   name: "Modal",
@@ -36,7 +43,7 @@ export default {
       categorieResume: {
         nom: "",
       },
-      tabCat: []
+      tabCat: [],
     };
   },
   methods: {
@@ -52,71 +59,107 @@ export default {
       document.querySelector("form").classList.add("blur");
     },
     afficherCat() {
-      axios.get('http://localhost:3001/api/categorie')
-      .then(res => {
-        // réponse sous forme de tableau
-        console.log(res.data);
-        let tab = res.data; 
-        // copie du tableau réponse dans tabCat, sur lequel on boucle dans le template
-        this.tabCat = tab.slice(0); 
-      })
-      .catch(error => {
-        console.error(error)
-      })
+      axios
+        .get("http://localhost:3001/api/categorie")
+        .then((res) => {
+          // réponse sous forme de tableau
+          console.log(res.data);
+          let tab = res.data;
+          // copie du tableau réponse dans tabCat, sur lequel on boucle dans le template
+          this.tabCat = tab.slice(0);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     },
     async ajoutCat() {
       try {
-        await axios.post('http://localhost:3001/api/categorie', {...this.categorieResume});
+        await axios.post("http://localhost:3001/api/categorie", {
+          ...this.categorieResume,
+        });
         console.log("categorie créée");
         this.categorieResume.nom = "";
         // on "rafraîchit" la liste des catégories:
         this.afficherCat();
-      } catch(e) {
+      } catch (e) {
         console.log(e);
       }
     },
     suppCat(idCat) {
-      axios.delete(`http://localhost:3001/api/categorie/${idCat}`)
-      .then(res => {
-        console.log(`${res.data} supprimé`);
-        // "recharge" la liste des catégories => affichage sans la catégorie supprimée
-        this.afficherCat();
-      })
-      .catch(error => console.error(error))
+      axios
+        .delete(`http://localhost:3001/api/categorie/${idCat}`)
+        .then((res) => {
+          console.log(`${res.data} supprimé`);
+          // "recharge" la liste des catégories => affichage sans la catégorie supprimée
+          this.afficherCat();
+        })
+        .catch((error) => console.error(error));
     },
     modifierCat(idCat) {
-      for(let i=0; i<this.tabCat.length; i++) {
-        if(this.tabCat[i]._id === idCat) {
-          axios.put(`http://localhost:3001/api/categorie/${idCat}`, {...this.categorieResume, nom: this.tabCat[i].nom})
-          .then(res => {
-            console.log(`${res} modifié`);
-          })
-          .catch(error => {
-            console.error(error);
-          })
+      for (let i = 0; i < this.tabCat.length; i++) {
+        if (this.tabCat[i]._id === idCat) {
+          axios
+            .put(`http://localhost:3001/api/categorie/${idCat}`, {
+              ...this.categorieResume,
+              nom: this.tabCat[i].nom,
+            })
+            .then((res) => {
+              console.log(`${res} modifié`);
+            })
+            .catch((error) => {
+              console.error(error);
+            });
         }
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
-
 <style scoped>
 .modal {
-    background-color: #fff;
-    width: 70%;
-    padding: 3%;
-    overflow-x: hidden;
-    overflow-y: auto;
-    position: fixed;
-    top: 30%;
-    left: 15%; 
-    z-index: 9;
+  background-color: #fff;
+  width: 70%;
+  padding: 3%;
+  overflow-x: hidden;
+  overflow-y: auto;
+  position: fixed;
+  top: 20%;
+  left: 15%;
+  z-index: 9;
+  height: 70vh;
+}
+.modal__body ul li {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
-.modal__body ul li {
-    display: flex;
-    justify-content: space-between;
+input {
+  margin: 20px 10px;
+  width: 30%;
+  height: 40px;
+  border-radius: 15px;
+  text-align: center;
+}
+button {
+  border-radius: 15px;
+  width: 80px;
+  height: 40px;
+  padding: 0;
+  cursor: pointer;
+}
+.ajoutCat {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.listeCat {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+img {
+  cursor: pointer;
 }
 </style>
