517e3c990ccf68a78afae2dc03a1779c944e9ce8
fabienD31 <fabien.desnoues31@gmail.com>
2021-07-08 11:08:36 +0200

    refactoBack fini

--- back/app.js
+++ back/app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.use('/api/card', cardRoutes);
-app.use('api/categorie', categoryRoutes);
+app.use('/api/categorie', categoryRoutes);
 
 
 
